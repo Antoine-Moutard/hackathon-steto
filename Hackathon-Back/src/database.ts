2e45020ac519f395ad54a014ad1ab683f28c19aa
purplegrn <amoutard368@gmail.com>
2024-02-05 14:33:50 +0100

    Ajout de la connexion BDD (à configurer)

---
+++ Hackathon-Back/src/database.ts
@@ -0,0 +1,20 @@
+import mysql from 'mysql2/promise';
+
+const connectDB = async () => {
+  try {
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    });
+
+    console.log('MySQL Connected');
+    return connection;
+  } catch (error) {
+    console.error('MySQL Connection Failed', error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;

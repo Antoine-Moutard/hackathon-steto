dbd8cf5c637268c445d0b9afbb2ebfd5c76568fd
purplegrn <amoutard368@gmail.com>
2024-02-05 15:19:27 +0100

    Update app.ts

--- Hackathon-Back/src/app.ts
+++ Hackathon-Back/src/app.ts
@@ -12,18 +12,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Serveur Express en cours d'exécution sur le port ${port}`);
 });
-
-
-const query = 'SELECT * FROM patient';
-
-
-async function executeQuery() {
-  try {
-    const [rows, fields] = await connection.execute(query);
-    // Traitez les résultats ici
-  } catch (error) {
-    // Gérez l'erreur ici
-  }
-}
-
-executeQuery()

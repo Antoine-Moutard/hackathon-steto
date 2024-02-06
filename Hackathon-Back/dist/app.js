886917c411c0d0f17382e6fff0baeefec7ba2980
purplegrn <amoutard368@gmail.com>
2024-02-06 15:00:20 +0100

    Fix user object

--- Hackathon-Back/dist/app.js
+++ Hackathon-Back/dist/app.js
@@ -10,11 +10,14 @@ const cors = require("cors");
 const app = (0, express_1.default)();
 const PORT = process.env.APP_PORT;
 //middleware pour accépter les requetes du frontend
-app.use(cors({
-    origin: "http://localhost:5173/",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-}));
+// Configuration des options CORS
+const corsOptions = {
+    origin: 'http://localhost:5173', // Remplacez cela par l'URL de votre front-end
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Autoriser les cookies et les en-têtes d'autorisation
+    optionsSuccessStatus: 204, // Répondre avec un code 204 si la requête CORS pré-vérification est réussie
+};
+app.use(cors(corsOptions));
 app.use(express_1.default.json());
 // Utilisez le routeur pour vos routes d'API
 app.use(routes_1.default);

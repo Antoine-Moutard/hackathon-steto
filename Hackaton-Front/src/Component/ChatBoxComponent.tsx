b8821fc91eb9861de6e83f2356cae13d54ee3a11
nathanSIO <janvnathan1@gmail.com>
2024-02-07 13:03:53 +0100

    ajout du form

--- Hackaton-Front/src/Component/ChatBoxComponent.tsx
+++ Hackaton-Front/src/Component/ChatBoxComponent.tsx
@@ -79,7 +79,7 @@ const ChatBoxComponent = ({patient, toggleChatBox}: ChatBoxComponentProps) => {
           {/* </p> */}
         </div>
         <div className="absolute inset-x-0 bottom-2 w-11/12 ml-5">
-          {/* <form className="flex"> */}
+          <form className="flex" onSubmit={(event) => event.preventDefault()}>
             <input
               id="messageInput"
               className="flex-1 p-2 border rounded-l-lg"
@@ -103,7 +103,7 @@ const ChatBoxComponent = ({patient, toggleChatBox}: ChatBoxComponentProps) => {
                 <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z" />
               </svg>
             </button>
-          {/* </form> */}
+          </form>
         </div>
       </div>
     </div>

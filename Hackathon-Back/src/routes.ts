f0dc0ac2e230c7a357ec6aae4c446ebf968ecd36
purplegrn <amoutard368@gmail.com>
2024-02-08 11:27:45 +0100

    fix routes v2

--- Hackathon-Back/src/routes.ts
+++ Hackathon-Back/src/routes.ts
@@ -152,7 +152,7 @@ router.get("/api/getMessageByPatientId/:id", async (req, res) => {
 
     // Executes the query
     const [messages] = await connection.query(
-      "SELECT m.content AS message_content, m.createdAt AS message_date, COALESCE(CONCAT(pr.firstname, ' ', pr.lastname), CONCAT(p.firstname, ' ', p.lastname)) AS sender_name, m.id, m.messageType -- Ajout pour déboguer FROM message m JOIN careteam c ON m.careTeamId = c.id LEFT JOIN practitioner pr ON m.senderId = pr.id LEFT JOIN patient p ON m.senderId = p.id WHERE c.subjectId = " + [req.params.id] + " and messageType = 'group' ORDER BY m.createdAt ASC");
+      "SELECT m.content AS message_content, m.createdAt AS message_date, COALESCE(CONCAT(pr.firstname, ' ', pr.lastname), CONCAT(p.firstname, ' ', p.lastname)) AS sender_name, m.id, m.messageType FROM message m JOIN careteam c ON m.careTeamId = c.id LEFT JOIN practitioner pr ON m.senderId = pr.id LEFT JOIN patient p ON m.senderId = p.id WHERE c.subjectId = "+ [req.params.id] +" and messageType = 'group' ORDER BY m.createdAt ASC" );
       
       // Closes the connection
     await connection.end();
